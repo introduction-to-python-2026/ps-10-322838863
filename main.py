3a05bb6b687038f66f0a5ba9fe4cf90ee29059d0
322838863 <shovalgos@gmail.com>
2026-01-03 19:51:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 
+
+from image_utils import load_image, edge_detection
+from skimage.filters import median
+from skimage.morphology import ball
+import matplotlib.pyplot as plt
+import numpy as np
+
+image = load_image("1000019832.jpg")
+clean_image = median(image, ball(3))
+edges = edge_detection(clean_image)
+threshold = np.mean(edges)
+binary_edges = edges > threshold
+
+plt.imshow(binary_edges, cmap='gray')
+plt.axis('off')
+plt.savefig("edges.png")
+plt.show()
+
